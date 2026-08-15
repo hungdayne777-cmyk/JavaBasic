2960b167adda4cafb2dfba14ce9e4432a667da9c
ThaiHung <thaihung12a3@gmail.comm>
2026-08-15 13:19:16 +0700

    Đã hoàn Lab03_QLSV có giao diện

---
+++ Lab03_QuanLySinhVien_GiaoDien/src/main/java/dao/Khoa.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dao;
+
+/**
+ *
+ * @author MSI
+ */
+public class Khoa {
+    private String MaKhoa;
+        private String TenKhoa;
+
+    public Khoa() {
+    }
+
+    public Khoa(String MaKhoa, String TenKhoa) {
+        this.MaKhoa = MaKhoa;
+        this.TenKhoa = TenKhoa;
+    }
+
+    public String getMaKhoa() {
+        return MaKhoa;
+    }
+
+    public void setMaKhoa(String MaKhoa) {
+        this.MaKhoa = MaKhoa;
+    }
+
+    public String getTenKhoa() {
+        return TenKhoa;
+    }
+
+    public void setTenKhoa(String TenKhoa) {
+        this.TenKhoa = TenKhoa;
+    }
+
+    @Override
+    public String toString() {
+        return this.TenKhoa;
+    }
+        
+}
---
+++ Lab03_QuanLySinhVien_GiaoDien/src/main/java/dao/KhoaDao.java
@@ -0,0 +1,109 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import java.util.List;
+import util.DBConection;
+
+/**
+ *
+ * @author MSI
+ */
+public class KhoaDao {
+
+    public List<Khoa> findAll() {
+        List<Khoa> ds = new ArrayList<>();
+        String sql = "SELECT * FROM KHOA";
+
+        try (Connection conn = DBConection.getConnection(); PreparedStatement ps = conn.prepareStatement(sql); ResultSet rs = ps.executeQuery()) {
+
+            while (rs.next()) {
+                String maKH = rs.getString("MaKhoa");
+                String tenKH = rs.getString("TenKhoa");
+
+                ds.add(new Khoa(maKH, tenKH));
+            }
+        } catch (SQLException e) {
+            System.out.println("Lỗi khi đọc dữ liệu: " + e.getMessage());
+        }
+        return ds;
+    }
+
+    public Khoa findById(String maKH) {
+        String sql = "SELECT * FROM KHOA WHERE MaKhoa=?";
+
+        try (Connection conn = DBConection.getConnection(); PreparedStatement ps = conn.prepareStatement(sql)) {
+
+            ps.setString(1, maKH);
+            try (ResultSet rs = ps.executeQuery()) {
+                if (rs.next()) {
+                    return new Khoa(
+                            rs.getString("MaKhoa"),
+                            rs.getString("TenKhoa")
+                    );
+                }
+            }
+        } catch (SQLException e) {
+            System.out.println("Lỗi khi tìm kiếm: " + e.getMessage());
+        }
+        return null;
+    }
+
+    public boolean delete(String maKH) {
+        String sql = "DELETE FROM KHOA WHERE MaKhoa=?";
+
+        try (Connection conn = DBConection.getConnection(); PreparedStatement ps = conn.prepareStatement(sql)) {
+
+            ps.setString(1, maKH);
+            int rows = ps.executeUpdate();
+            return rows > 0;
+
+        } catch (SQLException e) {
+            System.out.println("Lỗi khi xóa dữ liệu: " + e.getMessage());
+        }
+        return false;
+    }
+
+    public boolean insert(Khoa kh) {
+        String sql = "INSERT INTO KHOA VALUES (?, ?)";
+
+        try (Connection conn = DBConection.getConnection(); PreparedStatement ps = conn.prepareStatement(sql)) {
+
+            ps.setString(1, kh.getMaKhoa());
+            ps.setString(2, kh.getTenKhoa());
+
+            int rows = ps.executeUpdate();
+            return rows > 0;
+
+        } catch (Exception e) {
+            System.out.println("Lỗi khi thêm dữ liệu: " + e.getMessage());
+        }
+        return false;
+    }
+
+    public boolean update(Khoa kh) {
+        String sql = "UPDATE KHOA SET TenKhoa = ? WHERE MaKhoa = ?";
+
+        try (Connection conn = DBConection.getConnection(); PreparedStatement ps = conn.prepareStatement(sql)) {
+
+            ps.setString(1, kh.getTenKhoa());
+
+            ps.setString(2, kh.getMaKhoa());
+
+            int rows = ps.executeUpdate();
+            return rows > 0;
+
+        } catch (Exception e) {
+            System.out.println("Lỗi khi cập nhật dữ liệu: " + e.getMessage());
+        }
+        return false;
+    }
+}
